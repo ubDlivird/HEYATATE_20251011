f89ecb9bd8c339c2ba5315b8b32caf374d765e66
sh <articulation_kurenai@yahoo.co.jp>
2025-10-11 05:34:33 -0700

    Initial Commit

---
+++ HEYATATE_20251011/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HEYATATE_20251011
+//
+//  Created by shuya on R 7/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HEYATATE_20251011/HEYATATE_20251011App.swift
@@ -0,0 +1,17 @@
+//
+//  HEYATATE_20251011App.swift
+//  HEYATATE_20251011
+//
+//  Created by shuya on R 7/10/11.
+//
+
+import SwiftUI
+
+@main
+struct HEYATATE_20251011App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
